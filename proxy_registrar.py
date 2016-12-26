379ed9581bb3270dc56e8496e61e1ea6b0cfbdf0
lidiaenbla <lidiaeb1993@gmail.com>
2016-12-26 13:35:23 +0100

    guardado

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -40,6 +40,34 @@ class leerFicheroXml(ContentHandler):
     def get_tags(self):
         return self.misdatos
 
+class diccionarioRegistrar(socketserver.DatagramRequestHandler):
+    """
+    Echo server class
+    """
+    def handle(self):
+        """
+        Manejador
+        """
+        line = self.rfile.read()
+        print(line.decode('utf-8'))
+        linea = line.decode('utf-8').split()
+        if linea[0] == "INVITE":
+            if linea[1].split("@"):
+                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n SIP/2.0 180 Ring\r\n\r\n SIP/2.0 200 OK\r\n\r\n ")
+            else:
+                self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
+        elif linea[0] == "BYE":
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+        elif linea[0] == "REGISTER":
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+        elif linea[0] == "ACK":
+            # cancion = './mp32rtp -i 127.0.0.1 -p 23032 < ' + FILE
+            # print("vamos a ejecutar", cancion)
+            # os.system(cancion)
+            # print("hemos enviado la cancion")
+        else:
+            self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+
 parser = make_parser()
 cHandler = leerFicheroXml()
 parser.setContentHandler(cHandler)
@@ -69,4 +97,13 @@ for elementos in misdatos:
             elif etiqueta == 'log':
                 if atributo =='path':
                     pathLog = valor
+
+if __name__ == "__main__":
+
+    serv = socketserver.UDPServer((('', 5062)), diccionarioRegistrar)
+    print("Listening...")
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Finalizado proxy")
            
