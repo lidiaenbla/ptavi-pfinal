d906f29092151e440f304ce3023c2d630553ebf9
lidiaenbla <lidiaeb1993@gmail.com>
2017-01-02 12:50:50 +0100

    problemas con ACK

--- uaserver.py
+++ uaserver.py
@@ -8,11 +8,49 @@ import socketserver
 import sys
 import os
 
-try:
-    CONFIG = sys.argv[1]
-except IndexError:
-    sys.exit("Usage: python3 uaserver.py config")
+import socket
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+import socketserver
+import time
+import json
 
+class leerFicheroXml(ContentHandler):
+    def __init__(self):
+        self.account = ""
+        self.uaserver = ""
+        self.rtpaudio = ""
+        self.regproxy = ""
+        self.log = ""
+        self.audio = ""
+        self.misdatos = []
+
+    def startElement(self, etiqueta, attrs):
+
+        if etiqueta == 'account':
+            account = {'account': ({'username': attrs.get('username', ""),
+                                    'passwd': attrs.get('passwd', "")})}
+            self.misdatos.append(account)
+        elif etiqueta == 'uaserver':
+            uaserver = {'uaserver': ({'ip': attrs.get('ip', ""),
+                                      'puerto': attrs.get('puerto', "")})}
+            self.misdatos.append(uaserver)
+        elif etiqueta == 'rtpaudio':
+            rtpaudio = {'rtpaudio': ({'puerto': attrs.get('puerto', "")})}
+            self.misdatos.append(rtpaudio)
+        elif etiqueta == 'regproxy':
+            regproxy = {'regproxy': ({'ip': attrs.get('ip', ""),
+                                      'puerto': attrs.get('puerto', "")})}
+            self.misdatos.append(regproxy)
+        elif etiqueta == 'log':
+            log = {'log': ({'path': attrs.get('path', "")})}
+            self.misdatos.append(log)
+        elif etiqueta == 'audio':
+            audio = {'audio': ({'path': attrs.get('path', "")})}
+            self.misdatos.append(audio)
+
+    def get_tags(self):
+        return self.misdatos
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
@@ -42,7 +80,47 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
 if __name__ == "__main__":
 
-    serv = socketserver.UDPServer((('', 5060)), SIPRegisterHandler)
+    parser = make_parser()
+    cHandler = leerFicheroXml()
+    parser.setContentHandler(cHandler)
+
+    try:
+        CONFIG = sys.argv[1]
+    except IndexError:
+        sys.exit("Usage: python3 proxy_registrar.py config")
+
+    parser.parse(open(CONFIG))
+    misdatos = cHandler.get_tags()
+    for elementos in misdatos:
+        for etiqueta in elementos:
+            for atributo, valor in elementos[etiqueta].items():
+                if etiqueta == 'account':
+                    if atributo == 'username':
+                        username = valor
+                    elif atributo == 'passwd':
+                        passwd = valor
+                elif etiqueta == 'uaserver':
+                    if atributo =='ip':
+                        ip = valor
+                    elif atributo == 'puerto':
+                        puerto = valor
+                elif etiqueta == 'rtpaudio':
+                    if atributo == 'puerto':
+                        puertoRtp = valor
+                elif etiqueta == 'regproxy':
+                    if atributo == 'ip':
+                        ipProxy = valor
+                    elif atributo == 'puerto':
+                        puertoProxy = valor
+                elif etiqueta == 'log':
+                    if atributo == 'path':
+                        pathLog = valor
+                elif etiqueta == 'audio':
+                    if atributo == 'path':
+                        pathAudio = valor
+
+
+    serv = socketserver.UDPServer((('', int(puerto))), SIPRegisterHandler)
     print("Listening...")
     try:
         serv.serve_forever()
