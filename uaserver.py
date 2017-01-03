4ea72d7dce23b41edbadbb9573236d41ffc22560
lidiaenbla <lidiaeb1993@gmail.com>
2017-01-03 13:05:15 +0100

    solucionado puertoServer

--- uaserver.py
+++ uaserver.py
@@ -7,13 +7,27 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import socketserver
 import sys
 import os
-
 import socket
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 import socketserver
 import time
 import json
+
+def crearFichero(nombre):
+    fich = open(nombre, 'a+' )
+    fich.close()
+
+def rellenarFichero(nombre, evento):
+    nameFich = nombre + "Server.log"
+    crearFichero(nameFich)
+    fichLog = open(nameFich, 'a+')
+    horaActual = time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))
+    event = evento.split("\r\n")
+    EVNT = ""
+    for i in event:
+        EVNT = EVNT + str(i)
+    fichLog.write(str(horaActual) + " " + EVNT + "\r\n")
 
 class leerFicheroXml(ContentHandler):
     def __init__(self):
@@ -61,22 +75,34 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         Manejador
         """
         line = self.rfile.read()
-        print(line.decode('utf-8'))
+        print("Recibimos: ",line.decode('utf-8'))
         linea = line.decode('utf-8').split()
+        evento = "Received from " + ipProxy+":"+puertoProxy+": " + line.decode('utf-8')
+        rellenarFichero(username, evento)
         if linea[0] == "INVITE":
             if '@' in linea[1]:
-                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n SIP/2.0 180 Ring\r\n\r\n SIP/2.0 200 OK\r\n\r\n ")
+                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n SIP/2.0 180 Ring\r\n\r\n SIP/2.0 200 OK\r\n\r\n")
+                evento = "Sent to " + ipProxy+":"+puertoProxy+": SIP/2.0 100 Trying\r\n\r\n SIP/2.0 180 Ring\r\n\r\n SIP/2.0 200 OK\r\n\r\n" 
+                rellenarFichero(username, evento)
             else:
                 self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
+                evento = "Sent to " + ipProxy+":"+puertoProxy+": SIP/2.0 400 Bad Request\r\n\r\n" 
+                rellenarFichero(username, evento)
         elif linea[0] == "BYE":
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            evento = "Sent to " + ipProxy+":"+puertoProxy+": SIP/2.0 200 OK\r\n\r\n" 
+            rellenarFichero(username, evento)
         elif linea[0] == "ACK":
             cancion = './mp32rtp -i 127.0.0.1 -p 23032 < cancion.mp3'
             print("vamos a ejecutar", cancion)
             os.system(cancion)
             print("hemos enviado la cancion")
+            evento = "Sent to " +puertoRtp+": audio\r\n\r\n" 
+            rellenarFichero(username, evento)
         else:
             self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+            evento = "Sent to " + ipProxy+":"+puertoProxy+": SIP/2.0 405 Method Not Allowed\r\n\r\n" 
+            rellenarFichero(username, evento)
 
 if __name__ == "__main__":
 
@@ -119,10 +145,13 @@ if __name__ == "__main__":
                     if atributo == 'path':
                         pathAudio = valor
 
-
+    evento = "Starting..."
+    rellenarFichero(username, evento)
     serv = socketserver.UDPServer((('', int(puerto))), SIPRegisterHandler)
     print("Listening...")
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
+        evento = "Finishing."
+        rellenarFichero(username, evento)
         print("Finalizado servidor")
